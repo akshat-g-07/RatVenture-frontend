13ea36a700132c3dce51e542d45549ac59be0b4f
Akshat Garg <akshatg805@gmail.com>
2023-11-12 23:31:37 +0530

    10 App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import LandingPage from "./pages/landingPage/LandingPage";
+import AllPages from "./pages/AllPages";
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <AllPages />
     </>
   );
 }
