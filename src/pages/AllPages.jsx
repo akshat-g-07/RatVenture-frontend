c077d1177d35d2adc68c41665f3be2fa855fada2
Akshat Garg <akshatg805@gmail.com>
2023-11-16 22:22:10 +0530

    15 AllPages.jsx

--- src/pages/AllPages.jsx
+++ src/pages/AllPages.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./landingPage/LandingPage";
+import InputSize from "./inputPages/InputSize";
 
 const AllPages = () => {
   return (
@@ -9,6 +10,7 @@ const AllPages = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/inputSize" element={<InputSize />} />
         </Routes>
       </BrowserRouter>
     </>
